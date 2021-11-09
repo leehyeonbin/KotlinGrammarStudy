481a342a8403c2df9853d5a3d26caa140bbb0279
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2021-11-09 21:05:44 +0900

    객체 개념

---
+++ app/src/main/java/com/example/kotlinsgrammar/grammar/Class.kt
@@ -0,0 +1,24 @@
+package com.example.kotlinsgrammar.grammar
+
+
+
+// Class
+
+// OOP -> Object Oriented Programing (객체지향 프로그래밍)
+// 객체란?
+// - 이름이 있는 모든 것
+
+// 절차지향 프로그래밍
+// -> 코드를 위에서부터 아래로 실행하면 문제가 해결된다.
+
+// 객체지향 프로그래밍의 해결 방법
+// -> 객체를 생성하여 객체에게 일을 시켜서 해결한다.
+// - 선수, 심판, 관중, 경기장 -> 축구 게임
+
+// 객체를 생성하는 방법
+// - 설명서가 있어야 한다
+
+
+fun main(Array:String) {
+
+}
